e9a243989dccad62f9dd04bbeefa69c4f5e195a0
Liam <liamwerner777@gmail.com>
2023-06-08 22:08:10 +0200

    yeah about 1 4th

--- chess.go
+++ chess.go
@@ -1,59 +1,138 @@
 package main
 
 import (
-	"fmt"
-	. "gfx2"
-	"pieces"
+	"chess"
+	. "gfxw"
 )
 
 func main() {
-	var w_x, w_y uint16 = 800, 800
-	fmt.Println("starting game...")
+	var w_x, w_y uint16 = 904, 904
+	var a uint16 = calc_a(w_x, w_y)
+	var white_is_current_player bool = true
+
+	initialize(w_x, w_y, a)
+
+	var pieces [16]chess.Piece
+
+	pieces[0] = &chess.Rook{
+		ChessObject: chess.ChessObject{
+			Positioning: chess.Positioning{
+				Position: [2]uint16{5, 4},
+			},
+			White: true,
+		},
+	}
+
+	pieces[1] = &chess.Bishop{
+		ChessObject: chess.ChessObject{
+			Positioning: chess.Positioning{
+				Position: [2]uint16{5, 5},
+			},
+			White: false,
+		},
+	}
+
+	pieces[2] = &chess.Pawn{
+		ChessObject: chess.ChessObject{
+			Positioning: chess.Positioning{
+				Position: [2]uint16{1, 6},
+			},
+			White: false,
+		},
+	}
+
+	// fmt.Println(pieces[0].Give_Pos())
+
+	// pieces[0].Move_To([2]uint16{3, 4})
+
+	// fmt.Println(pieces[0].Give_Pos())
+
+	// legal := pieces[1].Give_Legal_Moves()
+	// fmt.Println(legal[0].Position[1])
+
+	draw_pieces(pieces, w_x, w_y, a)
+
+	for { //gameloop
+		button, status, m_x, m_y := MausLesen1()
+
+		if status == 1 && button == 1 {
+			var current_field [2]uint16 = calc_field(w_x, w_y, m_x, m_y)
+			var current_piece chess.Piece
+
+			for i := 0; i < len(pieces); i++ {
+				if pieces[i] != nil {
+					if current_field == pieces[i].Give_Pos() {
+						current_piece = pieces[i]
+					}
+				}
+			}
+
+			if current_piece != nil && current_piece.Is_White_Piece() == white_is_current_player { //wenn die maus ein piece angeklickt hat, welches dem aktuellen spieler gehört
+				current_piece.Calc_Moves(pieces)
+				var current_legal_moves [][2]uint16 = current_piece.Give_Legal_Moves()
+				for k := 0; k < len(current_legal_moves); k++ {
+					highlight(a, current_legal_moves[k])
+				}
+				// fmt.Println(current_piece.Give_Pos())
+			}
+		}
+	}
+}
+
+func initialize(w_x, w_y, a uint16) {
 	Fenster(w_x, w_y)
 	Fenstertitel("Chess")
 	Stiftfarbe(0, 255, 0)
 	Vollrechteck(0, 0, w_x, w_y)
-	draw_board(w_x, w_y)
-
-	pieces := make([]pieces.Piece, 8)
-	ROkk := &pieces.Knight{}
-	pieces[0] = &pieces.Rook{}
-
-	for { //gameloop
-		button, status, m_x, m_y := MausLesen1()
-		fmt.Println("button:", button, "status", status)
-
-		if status == 1 && button == 1 {
-			fmt.Println(calc_field(w_x, w_y, m_x, m_y))
-		}
-
-	}
-	// TastaturLesen1()
+	draw_board(a)
 }
 
-func draw_board(w_x, w_y uint16) {
+func calc_a(w_x, w_y uint16) uint16 {
 	var a uint16
 	if w_x < w_y {
 		a = w_x / 8
 	} else {
 		a = w_y / 8
 	}
+	return a
+}
+
+func draw_pieces(pieces [16]chess.Piece, w_x, w_y, a uint16) {
+	for i := 0; i < len(pieces); i++ {
+		if pieces[i] != nil {
+			chess.Draw(pieces[i], w_x, w_y, a)
+		}
+	}
+}
+
+func highlight(a uint16, pos [2]uint16) {
+
+	var cord_x uint16 = a * pos[0]
+	var cord_y uint16 = a * pos[1]
+
+	Transparenz(170)
+	Stiftfarbe(0, 255, 0)
+	Vollrechteck(cord_x, cord_y, a, a)
+	Transparenz(0)
+}
+
+func draw_board(a uint16) {
 	var f_x uint16 = 0
 	var f_y uint16 = 0
 	for i := 0; i <= 7; i++ {
 		for k := 0; k <= 7; k++ {
 			if k%2 == 0 {
 				if i%2 == 0 {
-					Stiftfarbe(255, 255, 255)
+					Stiftfarbe(240, 217, 181)
 				} else {
-					Stiftfarbe(0, 0, 0)
+					Stiftfarbe(181, 136, 99)
 				}
 
 			} else {
 				if i%2 == 1 {
-					Stiftfarbe(255, 255, 255)
+					Stiftfarbe(240, 217, 181)
 				} else {
-					Stiftfarbe(0, 0, 0)
+					Stiftfarbe(181, 136, 99)
 				}
 			}
 
@@ -65,7 +144,7 @@ func draw_board(w_x, w_y uint16) {
 	}
 }
 
-func calc_field(w_x, w_y, m_x, m_y uint16) (x, y uint16) {
+func calc_field(w_x, w_y, m_x, m_y uint16) [2]uint16 {
 	var a uint16
 	if w_x < w_y {
 		a = w_x / 8
@@ -73,10 +152,9 @@ func calc_field(w_x, w_y, m_x, m_y uint16) (x, y uint16) {
 		a = w_y / 8
 	}
 
-	var field_x uint16
-	var field_y uint16
+	var current_field [2]uint16
 
-	field_x = m_x / a
-	field_y = m_y / a
-	return field_x, field_y
+	current_field[0] = m_x / a
+	current_field[1] = m_y / a
+	return current_field
 }
