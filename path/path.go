6fd3c8c8a77c966022e8b2231b2fecc1ea24442c
Liam <liamwerner777@gmail.com>
2023-09-05 18:36:39 +0200

    created parser

--- path/path.go
+++ path/path.go
@@ -4,6 +4,6 @@ func Give_Path() string {
 	// return "H:\\Downloads\\Chess" //school
 	// return "C:\\Users\\liamw\\Desktop\\Chess"
 	//return "C:\\Users\\liamw\\Documents\\_Privat\\_Go\\Chess"
-	return "H:\\Dokumente\\Chess" //school new
-	//return "C:\\Users\\liamw\\Documents\\_Privat\\_Go\\Chess" //home
+	//return "H:\\Dokumente\\Chess" //school new
+	return "C:\\Users\\liamw\\Documents\\_Privat\\_Go\\Chess" //home
 }
